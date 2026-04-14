b7ced25bd7d236ebe28b4478a66c46abd94d1867
root <root@SattwiksASUS.>
2026-04-14 15:21:09 +0000

    14th April before vtz travel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from expense-tracker!")
+from fastapi import FastAPI
 
+app=FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return {'status': 'ok?'}
